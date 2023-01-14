2be5aa64e4dfd1ca7a272ee65418035fceb5bac3
Ritikg269 <ritikg269@gmail.com>
2023-01-14 21:45:17 +0530

    added baseurl

--- config.js
+++ config.js
@@ -3,8 +3,7 @@ const config={
     //local/demo
     // BASE_URL :'https://costsavingdev.quicktool.io', 
     // AUTH_BASE_URL:'https://authservicedev.quicktool.io',
-    AUTH_BASE_URL:'http://localhost:3001',
-    BASE_URL: 'http://localhost:5000',
+    BASE_URL: 'https://backend-zotq.onrender.com',
     // BASE_URL:'https://costsavingservice.quicktool.io',
     // AUTH_BASE_URL:'https://authservice.quicktool.io',
     ENV:'LOCAL'
