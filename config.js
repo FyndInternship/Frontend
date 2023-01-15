1f34e0ec0abfd3d8c2d33f2c1cb3f5ded91764ae
Ritikg269 <ritikg269@gmail.com>
2023-01-15 13:15:49 +0530

    prod backend

--- config.js
+++ config.js
@@ -1,13 +1,5 @@
 const config={
-
-    //local/demo
-    // BASE_URL :'https://costsavingdev.quicktool.io', 
-    // AUTH_BASE_URL:'https://authservicedev.quicktool.io',
-    AUTH_BASE_URL:'http://localhost:3001',
-    BASE_URL: 'http://localhost:5000',
-    // BASE_URL:'https://costsavingservice.quicktool.io',
-    // AUTH_BASE_URL:'https://authservice.quicktool.io',
+    BASE_URL: 'https://backend-zotq.onrender.com',
     ENV:'LOCAL'
 };
 export default config;
-
