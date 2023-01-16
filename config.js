8dfb38de6288b0d4913f5164e38978b61226c000
Ritikg269 <ritikg269@gmail.com>
2023-01-16 22:52:37 +0530

    crossdomain

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const config={
-    BASE_URL: 'http://localhost:5000',
+    BASE_URL: 'https://backend-zotq.onrender.com',
+    // BASE_URL: 'http://localhost:5000',
     ENV:'LOCAL'
 };
 export default config;
